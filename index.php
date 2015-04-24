06ea602ce9ae9ffe993424d18d30466056eac029
Stephanie Nando <stephaniesnando@gmail.com>
2015-04-24 11:21:53 -0700

    Day 31 Part 3

--- index.php
+++ index.php
@@ -29,7 +29,7 @@
                     
                     <div class='password'>
                         <label form='password'>Password</label>
-                        <input type='text' name='password' id='password'>
+                        <input type='password' name='password' id='password'>
                     </div>
                     
                     <button type='button' id='register'>Register</button>
@@ -111,6 +111,35 @@
                               me.state.change(me.state.PLAY);
                           }else{
                               alert(response);
+                          }
+                       })
+                       .fail(function(response){
+                          alert: ("Fail"); 
+                       });
+                    });
+                    
+                    $query("#load").bind("click", function(){
+                       $.ajax({
+                          type: "POST",
+                          url: "php/controller/login-user.php",
+                          data: {
+                              username: $('#username').val(),
+                              password: $('#passwoord').val()
+                          },
+                          dataType: "text"
+                       })
+                       .success(function(response){
+                          if(response === "Invalid username and password"){
+                              alert(response);
+                              
+                          }else{
+                              var data = jQuery.parseJSON(response);
+                              game.data.exp = data["exp"];
+                              game.data.exp1 = data["exp1"];
+                              game.data.exp2 = data["exp2"];
+                              game.data.exp3 = data["exp3"];
+                              game.data.exp4 = data["exp4"];
+                              me.state.change(me.state.SPENDEXP);
                           }
                        })
                        .fail(function(response){
